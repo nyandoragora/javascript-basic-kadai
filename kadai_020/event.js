c69ccfcd91cc379158b7916cd3e7bd4bbf2f3ccb
nyandoragora <kou.chocobo@gmail.com>
2025-04-14 16:32:22 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+const h2Text = document.getElementById("text");
+const btn = document.getElementById("btn");
+btn.addEventListener("click",() =>{
+    h2Text.textContent = "ボタンをクリックしました";
+});
+
+
+
+
